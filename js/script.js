745e9862566c2c8e866e0dce2a24e9025d7030fa
Pierluca-Strianese <pierluca.strianese@gmail.com>
2023-04-14 15:14:05 +0200

    add cicli for and if

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 const eleGrid = document.querySelector('.grid');
 
-for (let i = 0; i <= 100; i++){
-    eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${i}</div>`;
+for (let i = 1; i <= 100; i++){
+
+    if (i % 3 == 0) {
+        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell green">fizz</div>`;
+    }
+        else if (i % 5 == 0){
+            eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell yellow">buzz</div>`;
+        }
+            else if ((i % 3 == 0) && (i % 5 == 0)) {
+                eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell red">fizzbuzz</div>`;
+            }
+                else {
+                    eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${i}</div>`;
+                }
+    
 }
+
