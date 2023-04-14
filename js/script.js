935d5b6f2dc064dc901b3c6971ddfcb2370a9870
Pierluca-Strianese <pierluca.strianese@gmail.com>
2023-04-14 15:17:02 +0200

    fix if i%3 & i%5

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@ const eleGrid = document.querySelector('.grid');
 
 for (let i = 1; i <= 100; i++){
 
-    if (i % 3 == 0) {
-        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell green">fizz</div>`;
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell red">fizzbuzz</div>`;
     }
         else if (i % 5 == 0){
             eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell yellow">buzz</div>`;
         }
-            else if ((i % 3 == 0) && (i % 5 == 0)) {
-                eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell red">fizzbuzz</div>`;
+            else if (i % 3 == 0) {
+                eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell green">fizz</div>`;
             }
                 else {
                     eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${i}</div>`;
